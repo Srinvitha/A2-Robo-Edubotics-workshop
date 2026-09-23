1b87dd07a405550d6de80a4a8b4fff0d6bd3a7c7
Srinvitha <srinvitha.nutakki@gmail.com>
2026-09-23 13:54:06 +0530

    Python Practice and New Concept Explorations

--- PythonPractice/main.py
+++ PythonPractice/main.py
@@ -1,11 +1,11 @@
 # main.py
 # imports and uses the module above
 # 12
-from helpers import is_prime, is_palindrome
+from PythonPractice.helpers import is_prime, is_palindrome
 
 print(is_prime(17))
 print(is_palindrome("radar"))
 
-import helpers
+import PythonPractice.helpers as helpers
 
 print(helpers.is_prime(18))
